1817458668e487d3358b9688f3094ee3c60c33ed
TheIllusiveC4 <itwasmec4@gmail.com>
2020-12-17 21:53:50 -0800

    update copyright

--- src/main/java/top/theillusivec4/polymorph/loader/common/integration/ironfurnaces/IronFurnacesModule.java
+++ src/main/java/top/theillusivec4/polymorph/loader/common/integration/ironfurnaces/IronFurnacesModule.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2020 C4
+ *
+ * This file is part of Polymorph, a mod made for Minecraft.
+ *
+ * Polymorph is free software: you can redistribute it and/or modify it under the terms of the GNU
+ * Lesser General Public License as published by the Free Software Foundation, either version 3 of
+ * the License, or any later version.
+ *
+ * Polymorph is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with Polymorph.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package top.theillusivec4.polymorph.loader.common.integration.ironfurnaces;
 
 import ironfurnaces.container.BlockIronFurnaceScreenHandler;
@@ -162,6 +179,7 @@ public class IronFurnacesModule extends CompatibilityModule {
       this.input = container.slots.get(0).inventory;
     }
 
+    @SuppressWarnings("unchecked")
     private RecipeType<? extends AbstractCookingRecipe> getRecipeType() {
 
       if (this.input instanceof BlockIronFurnaceTileBase) {
