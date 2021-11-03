c8f59b8de05d7e484505067f4587c556be619dfb
TheIllusiveC4 <29991504+TheIllusiveC4@users.noreply.github.com>
2021-11-02 17:47:36 -0700

    update copyright

--- forge/src/main/java/top/theillusivec4/polymorph/mixin/integration/craftingstation/MixinCraftingStationContainer.java
+++ forge/src/main/java/top/theillusivec4/polymorph/mixin/integration/craftingstation/MixinCraftingStationContainer.java
@@ -1,3 +1,24 @@
+/*
+ * Copyright (C) 2020-2021 C4
+ *
+ * This file is part of Polymorph.
+ *
+ * Polymorph is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Polymorph is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * and the GNU Lesser General Public License along with Polymorph.
+ * If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
 package top.theillusivec4.polymorph.mixin.integration.craftingstation;
 
 import com.tfar.craftingstation.CraftingStationContainer;
