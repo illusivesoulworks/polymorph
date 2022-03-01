bcf8146d436e6fa5d4f7592cfd515a606e855e75
TheIllusiveC4 <29991504+TheIllusiveC4@users.noreply.github.com>
2022-03-01 01:08:17 -0800

    fix RS crash

--- forge/src/main/java/top/theillusivec4/polymorph/common/integration/refinedstorage/GridTileRecipeData.java
+++ forge/src/main/java/top/theillusivec4/polymorph/common/integration/refinedstorage/GridTileRecipeData.java
@@ -25,6 +25,7 @@ import com.refinedmods.refinedstorage.tile.grid.GridTile;
 import net.minecraft.inventory.CraftingInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.NonNullList;
+import net.minecraft.world.World;
 import top.theillusivec4.polymorph.common.capability.AbstractTileEntityRecipeData;
 
 public class GridTileRecipeData extends AbstractTileEntityRecipeData<GridTile> {
@@ -35,16 +36,21 @@ public class GridTileRecipeData extends AbstractTileEntityRecipeData<GridTile> {
 
   @Override
   protected NonNullList<ItemStack> getInput() {
-    CraftingInventory craftingInventory = this.getOwner().getNode().getCraftingMatrix();
+    GridTile gridTile = this.getOwner();
+    World world = gridTile.getWorld();
 
-    if (craftingInventory != null) {
-      NonNullList<ItemStack> stacks =
-          NonNullList.withSize(craftingInventory.getSizeInventory(), ItemStack.EMPTY);
+    if (world != null) {
+      CraftingInventory craftingInventory = gridTile.getNode().getCraftingMatrix();
 
-      for (int i = 0; i < craftingInventory.getSizeInventory(); i++) {
-        stacks.set(i, craftingInventory.getStackInSlot(i));
+      if (craftingInventory != null) {
+        NonNullList<ItemStack> stacks =
+            NonNullList.withSize(craftingInventory.getSizeInventory(), ItemStack.EMPTY);
+
+        for (int i = 0; i < craftingInventory.getSizeInventory(); i++) {
+          stacks.set(i, craftingInventory.getStackInSlot(i));
+        }
+        return stacks;
       }
-      return stacks;
     }
     return NonNullList.create();
   }
